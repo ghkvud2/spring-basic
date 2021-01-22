ee3b448703efd3b634da2c09e281735de7c7f400
ghkvud2 <ghkvud2@naver.com>
2021-01-23 00:08:56 +0900

    RestController

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+	@ResponseBody
 	@RequestMapping("index")
 	public String index() {
-
-		return "root.index";
+		return "Hello1";
+//		return "root.index";
 	}
 }
